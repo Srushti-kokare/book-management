b12666b85f9d01efc9670056c7d09c8c8d1b7934
srushti <84494793+Srushti-kokare@users.noreply.github.com>
2022-05-16 22:23:06 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,63 +1,38 @@
 const jwt = require("jsonwebtoken");
-const bookModel = require("../model/bookModel")
-const ObjectId = require('mongoose').Types.ObjectId
+const bookModel = require("../model/bookModel");
 
 
+const authentication = async (req, res, next) => {
+    try {
+        let token = req.headers["x-api-key"];
+        if (!token) {
+            return res.status(401).send({ status: false, message: "important header missing" })
+        }
+        let decodedToken = jwt.verify(token, 'projectthreebook')
+        if (!decodedToken) return res.status(401).send({ status: false, message: 'token is not valid' })
 
-const authenticationUser = function (req, res, next){
-        try {
-           let token = req.headers["x-api-key"];
-           if (!token) return res.status(401).send({ status: false, msg: "token must be present" });
-           let decodedToken = jwt.verify(token,"group16",{ignoreExpiration:true});
-           if (!decodedToken)return res.status(400).send({ status: false, msg: "token is invalid" }); // this verify the token that token is correct or not
-           let a = Math.floor(Date.now()/1000)
-           if(decodedToken.exp<a)return res.status(401).send({ status: false, msg: "token is expired" });
-           next()
-        }catch (err) {
-           res.status(500).send({ status: false, Error: err.message });
-        }}
-     
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+    next()
+}
 
-
- const authorisationUser = async function(req, res, next) {
-        try{
-            let bookToBeModified = req.body.userId
-            let token = req.headers["x-api-key"]
-            let decodedToken = jwt.verify(token,"group16")
-            let userLoggedIn = decodedToken.userId
-            if (bookToBeModified != userLoggedIn) return res.status(400).send({ status: false, msg: "authentication denied" })
-            next()
-          }catch (err) {
-            res.status(500).send({ status: false, Error: err });
-         }}
-
- const bookAuthorise = async function(req,res,next){ 
-        try{     
-            let token = req.headers["x-api-key"];
-            let bookId = req.params.bookId;
-            if(!ObjectId.isValid(bookId)) return res.status(400).send({status:false,msg:"Invalid Book Id"})
-            let bookDetails = await bookModel.findById(bookId)
-            let userId = bookDetails.userId
-            let decodedToken = jwt.verify(token, "group16");
-            if (!decodedToken)return res.status(400).send({ status: false, msg: "token is invalid" });
-            let decoded = decodedToken.userId
-            if (userId != decoded)return res.status(400).send({ status: false, msg: "anthentication denied" })
-         next()
-      } 
-      catch (err) {
-         res.status(500).send({ status: false, Error: err.message });
-      }
-   }
-      
+const authorisation = async (req, res, next) => {
+    try {
+        let token = req.headers["x-api-key"];
+        let decodedToken = jwt.verify(token, "projectthreebook");
+        let userLoggingIn = req.params.bookId
+        let userLoggedIn = decodedToken.id
+        let value = await bookModel.findById(userLoggingIn)
+        if (!value) return res.status(404).send({ status: false, message: "book not found" })
+        if (value.userId != userLoggedIn) return res.status(403).send({ status: false, message: "You are not allowed to modify requested book data" })
+    }
+    catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
+    }
+    next()
+}
 
 
-
-
-
-
-
-
-
-module.exports.authenticationUser  = authenticationUser ;
-module.exports.authorisationUser = authorisationUser;
-module.exports.bookAuthorise = bookAuthorise;
+module.exports.authentication = authentication
+module.exports.authorisation = authorisation
