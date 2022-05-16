ec2dc9b0e1f106d1e9fbe6332e2f6224fcd2a59c
srushti <84494793+Srushti-kokare@users.noreply.github.com>
2022-05-16 22:22:21 +0530

    Update route.js

--- src/Route/route.js
+++ src/Route/route.js
@@ -1,25 +1,37 @@
-const express = require('express');
-const router = express.Router();
-const userController=require("../controller/UserController")
-const bookController=require("../controller/BookController")
-const reviewController=require("../controller/ReviewController")
-const Middleware=require("../middleware/auth")
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////
+const express = require("express")
+const router = express.Router()
 
-router.post("/register",userController.createUser)
-router.post("/login",userController.loginUser)
-router.post("/books",Middleware.authenticationUser,Middleware.authorisationUser,bookController.createBook)
-router.get("/books",Middleware.authenticationUser,bookController.getBooksbyQuery)
-router.delete("/books/:bookId",Middleware.authenticationUser,Middleware.bookAuthorise,bookController.deleteById)
-router.put("/books/:bookId" ,Middleware.authenticationUser,Middleware.bookAuthorise,bookController.updateBooks)
-router.get("/books/:bookId",Middleware.authenticationUser,Middleware.bookAuthorise,bookController.bookDetails)
-router.post("/books/:bookId/review",reviewController.review)
-router.put("/books/:bookId/review/:reviewId",reviewController.updateReview)
-router.delete("/books/:bookId/review/:reviewId",reviewController.Deletereview)
+const userController = require("../controllers/userController")
+const bookController = require("../controllers/bookController")
+const reviewController = require("../controllers/reviewController")
+
+const middleware = require("../middleware/auth")
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// user
+router.post("/register", userController.register)
+
+router.post("/login", userController.login)
+
+// book
+router.post('/books', middleware.authentication, bookController.createBook)
+
+router.get("/books", middleware.authentication, bookController.getBook)
+
+router.get("/books/:bookId", middleware.authentication, bookController.getBooksById)
+
+router.put("/books/:bookId", middleware.authentication, middleware.authorisation, bookController.updateBook)
+
+router.delete("/books/:bookId", middleware.authentication, middleware.authorisation, bookController.deleteBook)
+
+// review
+router.post("/books/:bookId/review", reviewController.createReview)
+
+router.put("/books/:bookId/review/:reviewId", reviewController.updateReview)
+
+router.delete("/books/:bookId/review/:reviewId", reviewController.deleteReview)
 
 
 module.exports = router;
