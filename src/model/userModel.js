9a6d1f526765ef36b24729b5efc8c2536246cdfd
srushti <84494793+Srushti-kokare@users.noreply.github.com>
2022-05-16 22:25:38 +0530

    Update userModel.js

--- src/model/userModel.js
+++ src/model/userModel.js
@@ -1,38 +1,36 @@
-const mongoose = require('mongoose');
-// ObjectId = mongoose.Schema.Types.ObjectId
+const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema( {
-   title:{
-       type:String,
-       required:true,
-       enum:["Mr", "Mrs", "Miss"]
-
-   },
-    name:{
-        type:String,
-        required:true
+const userSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        enum: ['Mr', 'Mrs', 'Miss'],
+        required: true
     },
-    phone:{type:String,
-        required:true,
-        unique:true
+    name: {
+        type: String,
+        required: true
     },
-   email:{
-       type:String,
-        required:true,
-            unique:true,
-            trim:true
-   },
-   password:{
-    type:String,
-     required:true,
-    
-},
-    address:{
-        street:{type:String},
-        city:{type:String},
-        pincode:{type:String}
-    }, 
-    
+    phone: {
+        type: String,
+        requried: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        requried: true,
+        Unique: true
+    },
+    password: {
+        type: String,
+        requried: true,
+        Unique: true
+    },
+    address: {
+        street: { type: String },
+        city: { type: String },
+        pincode: { type: String }
+    }
+}, { timestamps: true })
 
-},{timestamps:true});
-module.exports = mongoose.model("User", userSchema)
+
+module.exports = mongoose.model('User', userSchema)
